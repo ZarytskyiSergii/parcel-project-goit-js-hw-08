81725f83dd53887aabf620d244311c02754f6bd9
Sergii <sonik2552@gmail.com>
2023-03-25 16:28:56 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,24 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
-const items = galleryItems.map(item => {
-  return `<div class="gallery__item">
-  <a class="gallery__link" href="${item.original}" >
-  <img
-    class="gallery__image"
-    src="${item.preview}"
-    data-source="${item.original}"
-    alt="${item.description}"
-  />
-</a>
-</div>`}).join("");
+const gallery = document.querySelector('.gallery');
 
-const galleryBox = document.querySelector('.gallery');
-galleryBox.innerHTML = items;
+gallery.innerHTML = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <li>
+    <a class='gallery__item' href="${original}">
+    <img class="gallery__image" src="${preview}" alt="$description" />
+    </a></li>`;
+})
+    .join("");
 
-galleryBox.addEventListener('click', onLinkClick);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelaay: 250,
+    })
 
-function onLinkClick(event) {
-    event.preventDefault();
-	const instance = basicLightbox.create(`
-		<img width="1400" height="900" src="${event.target.dataset.source}">
-	`)
-	instance.show()
-}
 console.log(galleryItems);
